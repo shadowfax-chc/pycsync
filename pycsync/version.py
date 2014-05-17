2f5d640de4a26498ec11f580b81d8ea58f585097
Timothy Messier <tim.messier@gmail.com>
2014-05-17 13:48:39 -0400

    Target next version

--- pycsync/version.py
+++ pycsync/version.py
@@ -7,7 +7,7 @@ Get version of pycsync
 '''
 
 from distutils.version import StrictVersion  # pylint: disable=E0611,F0401
-__version__ = str(StrictVersion('0.1b0'))
+__version__ = str(StrictVersion('0.1b1'))
 del StrictVersion
 
 if __name__ == '__main__':
